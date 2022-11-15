d3cea175fa5f90a1469afbd769b31971dd946b1a
mouloudelarram <78663674+mouloudelarram@users.noreply.github.com>
2022-11-15 10:31:38 +0100

    Update test.asm

--- test.asm
+++ test.asm
@@ -267,4 +267,71 @@
 		li $v0,10
 		syscall
 	
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+.data
+message_1: .asciiz "Enter n = "
+message_2: .asciiz "La valeur de  Fibonacci est : "
+
+.text
+
+	li $v0, 4
+	la $a0, message_1
+	syscall
+	li $v0, 5
+	syscall
+	
+	move $a0, $v0
+	jal fibonacci
+	move $a1, $v0 
+
+	li $v0, 4
+	la $a0, message_2
+	syscall
+
+	li $v0, 1
+	move $a0, $a1
+	syscall
+
+	# Exit
+	li $v0, 10
+	syscall
+
+fibonacci:
+
+	addi $sp, $sp, -12
+	sw $ra, 8($sp)
+	sw $s0, 4($sp)
+	sw $s1, 0($sp)
+	move $s0, $a0
+	li $v0, 1 
+	ble $s0, 0x2, fibonacciExit 
+	addi $a0, $s0, -1 
+	jal fibonacci
+	move $s1, $v0 
+	addi $a0, $s0, -2 
+	jal fibonacci
+	add $v0, $s1, $v0 
+	
+fibonacciExit:
+	
+	lw $ra, 8($sp)
+	lw $s0, 4($sp)
+	lw $s1, 0($sp)
+	addi $sp, $sp, 12
+	jr $ra
+
